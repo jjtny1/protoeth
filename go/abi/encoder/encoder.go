0cd9a7d8651c886b09b6d215766728750829c39d
Joby Thundil <jjtny1@gmail.com>
2022-03-27 20:01:11 -0400

    move utils to own file

--- go/abi/encoder/encoder.go
+++ go/abi/encoder/encoder.go
@@ -235,27 +235,3 @@ func encodeFunction(buffer bytes.Buffer, function *solidity.Function) error {
 
 	return nil
 }
-
-func leftPadByteSliceZero32(byteSlice []byte) []byte {
-	return leftPadByteSliceToSizeWithZeros(byteSlice, 32)
-}
-
-func leftPadBytSliceFF32(byteSlice []byte) []byte {
-	leftPaddedBytes := make([]byte, 32)
-	for i := 0; i < len(leftPaddedBytes); i = i + 1 {
-		leftPaddedBytes[i] = 0xff
-	}
-
-	copy(leftPaddedBytes[32-len(byteSlice):], byteSlice)
-	return leftPaddedBytes
-}
-
-func leftPadByteSliceToSizeWithZeros(byteSlice []byte, size int) []byte {
-	if size <= len(byteSlice) {
-		return byteSlice
-	}
-
-	leftPaddedBytes := make([]byte, size)
-	copy(leftPaddedBytes[size-len(byteSlice):], byteSlice)
-	return leftPaddedBytes
-}
